c65b832fb7c773096d15642562afc72884a445fc
vavrek <vavrek@vavrek.com>
2020-11-02 09:58:09 +0100

    Update good.sh

--- mind/stella/words/good.sh
+++ mind/stella/words/good.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# OpenAssistant 0.04
-# 2016 General Public License V3
-# By Andrew Vavrek, Clayton G. Hobbs, Jezra, Jonathan Kulp
-
 # Shuffle Script By Jonathan Kulp . http://jonathankulp.org
 
 # good.sh
